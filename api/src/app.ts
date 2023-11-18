20ed9b3a39aa9e6c2d81cc4adb772d35a271bd7b
tahaluh <thauanlucascpl@gmail.com>
2023-11-18 11:58:54 -0300

    initial: api setup

---
+++ api/src/app.ts
@@ -0,0 +1,11 @@
+import express from "express";
+import cors from "cors";
+
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+
+app.get("/test", (req, res) => res.status(200).send("Teste de rota"));
+
+export default app;
